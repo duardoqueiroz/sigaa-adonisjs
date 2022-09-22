36b250c47b976af64601dd31c4cfea3567e7d12c
duardoqueiroz <eduardoldq1@gmail.com>
2022-09-22 10:42:40 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  purge: [
+    './resources/views/**/*.edge',
+    './resources/css/**/*.css',
+    './resources/js/**/*.js',
+    './resources/js/**/*.ts',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
