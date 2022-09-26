0e36edcb580226c5f0af188ce203c1f8f4e85923
duardoqueiroz <eduardoldq1@gmail.com>
2022-09-26 15:07:24 -0300

    chore: add new user columns to registration

---
+++ database/migrations/1664210763312_add_all_user_columns.ts
@@ -0,0 +1,50 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import { GENDERS } from 'Contracts/interfaces/GENDERS'
+import { MARITAL_STATUS } from 'Contracts/interfaces/MARITAL_STATUS'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('name').notNullable()
+      table.string('cpf').notNullable().unique()
+      table.string('username').notNullable().unique()
+      table.string('phone').notNullable().unique()
+      table.enum('genre', [GENDERS.ALL, GENDERS.FEMALE, GENDERS.MALE, GENDERS.OTHERS]).notNullable()
+      table.date('birthday').notNullable().defaultTo(new Date().toISOString())
+      table.enum('marital_status', [
+        MARITAL_STATUS.DIVORCED,
+        MARITAL_STATUS.ENGAGED,
+        MARITAL_STATUS.MARRIED,
+        MARITAL_STATUS.SINGLE,
+        MARITAL_STATUS.WIDOWED,
+        MARITAL_STATUS.SEPARATED,
+        MARITAL_STATUS.OTHER,
+      ])
+      table.string('country')
+      table.string('race')
+      table.string('nationality')
+      table.string('naturalness')
+      table.boolean('confirmed').defaultTo(false)
+    })
+  }
+
+  public async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumns(
+        'name',
+        'cpf',
+        'username',
+        'phone',
+        'genre',
+        'birthday',
+        'marital_status',
+        'country',
+        'race',
+        'nationality',
+        'naturalness'
+      )
+    })
+  }
+}
